6bc2b0f1e4ddadd53c9cc4342caaa739ed225fb8
LOLAKU <159569758+LOLAKU@users.noreply.github.com>
2024-02-16 15:33:01 +0000

    уже получается 2

--- src/Components/PrimaryButton.js
+++ src/Components/PrimaryButton.js
@@ -5,13 +5,14 @@ import styled from "styled-components";
 
 function PrimaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
-    let minWidth = props.minWidth === undefined ? '144px' : props.minWidth; 
+    let minWidth = props.minWidth === undefined ? '217px' : props.minWidth; 
     const Button = styled.div`
-        background: #4B759E;
-        border: 1px solid #152842;
+        background: #FFFF;
+        border-radius: 20px;
+        border: 1px solid #64748B;
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
-        color: white;
+        color: #64748B;
         &:hover {
             background: linear-gradient(180deg, #4D9FF0 0%, #36699D 100%);
         }
@@ -21,7 +22,7 @@ function PrimaryButton(props) {
     `;
     return (
         <Clickable>
-            <Button className='p-2 text-md'>
+            <Button className='p-2 text-rg'>
                 {props.children}
             </Button>
         </Clickable>
