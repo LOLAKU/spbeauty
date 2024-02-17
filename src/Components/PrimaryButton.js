fdd50f35c55a3be6c75f8feebe4be3e473d3cf8f
LOLAKU <159569758+LOLAKU@users.noreply.github.com>
2024-02-17 16:30:12 +0000

    гоним5

--- src/Components/PrimaryButton.js
+++ src/Components/PrimaryButton.js
@@ -5,19 +5,18 @@ function PrimaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
     let minWidth = props.minWidth === undefined ? '217px' : props.minWidth; 
     const Button = styled.div`
-        background: #FFFF;
-        border-radius: 20px;
-        border: 1px solid #64748B;
-        box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
-        min-width: ${minWidth};
-        color: #64748B;
-        &:hover {
-            background: #E2E8F0;
-        }
-        &:active {
-            background: #94A3B8;
-        }
-    `;
+    background: #3B82F6;
+    border-radius: 20px;
+    box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
+    min-width: ${minWidth};
+    color: #FFFF;
+    &:hover {
+        background: #72A4F4;
+    }
+    &:active {
+        background: #004CC7;
+    }
+`;
     return (
         <Clickable>
             <Button className='p-2 text-rg'>
--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -3,19 +3,22 @@ import styled from "styled-components";
 
 function SecondaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
-    let minWidth = props.minWidth === undefined ? '160px' : props.minWidth;
+    let minWidth = props.minWidth === undefined ? '217px' : props.minWidth;
     const Button = styled.div`
-        border-radius: 20px;
-        box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
-        min-width: ${minWidth};
-        color: #FFFF;
+            background: #FFFF;
+            border-radius: 20px;
+            border: 1px solid #64748B;
+            box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
+            min-width: ${minWidth};
+            color: #64748B;
         &:hover {
-            background: #72A4F4;
-        }
-        &:active {
-            background: #004CC7;
-        }
-    `;
+             background: #E2E8F0;
+             }
+         &:active {
+             background: #94A3B8;
+             }
+`;
+
     return (
         <Clickable >
             <Button className='p-2 text-rg'>
